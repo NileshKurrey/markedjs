06fa6c8419a6d407b7664d58b06a7c7c3ede3614
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-03-28 21:02:15 +0530

    Intial commit

--- script.js
+++ script.js
@@ -6,6 +6,5 @@ let OutputSection = document.getElementById('OutputSection')
 
 function input(){
   let inputValue = String(inputText.value)
-  Inputparse(inputValue)
   OutputSection.innerHTML = marked.parse(inputValue) +'\n'
 }
