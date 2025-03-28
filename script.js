dea9c989b957fe21bb45bdd6e1e2ec1fc7fcd7c2
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-03-28 20:59:29 +0530

    intial commit

--- script.js
+++ script.js
@@ -4,18 +4,8 @@ let OutputSection = document.getElementById('OutputSection')
 
 
 
-function Inputparse(text){
-    let textArr = text.split('\n')
-    textArr.forEach(text => {
-        let textEl = String(text)
-        if(textEl.startsWith('#')){
-            
-        }
-    });
-    // console.log(textArr)
-}
 function input(){
   let inputValue = String(inputText.value)
   Inputparse(inputValue)
-//   OutputSection.innerHTML = marked.parse(inputValue) +'\n'
+  OutputSection.innerHTML = marked.parse(inputValue) +'\n'
 }
